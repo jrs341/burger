076fbe32ed98839a55967d119dc36a0be42b7373
Jason Sinn <jrs341@me.com>
2016-11-06 16:42:03 -0600

    added connection java script to connection file however not connecting yet

--- server.js
+++ server.js
@@ -1,60 +1,32 @@
-// ==============================================================================
-// DEPENDENCIES
-// Series of npm packages that we will use to give our server useful functionality
-// ==============================================================================
+/*
+Here is where you set up your server file.
+express middleware.
+*/
 
 var express = require('express');
-var mehodOverride = require('method-override')
 var bodyParser = require('body-parser');
-var path = require('path');
+var methodOverride = require('method-override');
 
+var app = express();
 
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + '/public'));
 
+app.use(bodyParser.urlencoded({
+	extended: false
+}));
+// override with POST having ?_method=DELETE
+app.use(methodOverride('_method'));
+var exphbs = require('express-handlebars');
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main'
+}));
+app.set('view engine', 'handlebars');
 
+var routes = require('./controllers/burgers_controller.js');
+app.use('/', routes);
 
+var port = 8080;
 
-// ==============================================================================
-// EXPRESS CONFIGURATION
-// This sets up the basic properties for our express server 
-// ==============================================================================
-
-var app = express(); // Tells node that we are creating an "express" server
-var PORT = process.env.PORT || 8080; // Sets an initial port. We'll use this later in our listener
-
-// BodyParser makes it easy for our server to interpret data sent to it.
-// The code below is pretty standard.
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(bodyParser.text());
-app.use(bodyParser.json({type:'application/vnd.api+json'}));
-
-
-
-
-
-
-
-// ================================================================================
-// ROUTER
-// The below points our server to a series of "route" files.
-// These routes give our server a "map" of how to respond when users visit or request data from various URLs. 
-// ================================================================================
-
-// require('./app/routing/api-routes.js')(app); 
-// require('./app/routing/html-routes.js')(app);
-
-
-
-
-
-
-
-
-// ==============================================================================
-// LISTENER
-// The below code effectively "starts" our server 
-// ==============================================================================
-
-app.listen(PORT, function() {
-	console.log("App listening on PORT: " + PORT);
-});
+app.listen(port);
+// console.log('listening on port ' + port);

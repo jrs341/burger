bf9c3e36e75bf4d24b54fba5c6bed82bf88a9c43
Jason Sinn <jrs341@me.com>
2016-11-08 15:45:58 -0600

    updted the orm file added orm to the burger file and updated the burgers controller file currently is not accessing the database having an access issue on my computer

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -3,33 +3,33 @@ Here is where you create all the functions that will do the routing for your app
 */
 var express = require('express');
 var router = express.Router();
-var burgers = require('../models/burger.js');
+var burger = require('../models/burger.js');
 
 router.get('/', function (req, res) {
 	res.redirect('/burgers');
 });
 
-router.get('/cats', function (req, res) {
-	cat.all(function (data) {
+router.get('/burgers', function (req, res) {
+	burger.all(function (data) {
 		var hbsObject = { burgers: data };
 		console.log(hbsObject);
 		res.render('index', hbsObject);
 	});
 });
 
-router.post('/cats/create', function (req, res) {
-	cat.create(['name', 'sleepy'], [req.body.name, req.body.sleepy], function () {
-		res.redirect('/cats');
+router.post('/burgers/create', function (req, res) {
+	burger.create(['burger_name'], [req.body.burger_name], function () {
+		res.redirect('/burgers');
 	});
 });
 
-router.put('/cats/update/:id', function (req, res) {
+router.put('/burgers/update/:id', function (req, res) {
 	var condition = 'id = ' + req.params.id;
 
 	console.log('condition', condition);
 
-	cat.update({ sleepy: req.body.sleepy }, condition, function () {
-		res.redirect('/cats');
+	burgers.update({ burger_name: req.body.name }, condition, function () {
+		res.redirect('/burgers');
 	});
 });
 

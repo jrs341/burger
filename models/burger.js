d4cdb3959d5543323d568b28b270d1d09751b138
Jason Sinn <jrs341@me.com>
2016-11-14 16:46:35 -0600

    server is working burger image and test html are rendering

--- models/burger.js
+++ models/burger.js
@@ -22,7 +22,7 @@ var burger = {
 		});
 	},
 	delete: function (condition, cb) {
-		orm.delete('cats', condition, function (res) {
+		orm.delete('burgers', condition, function (res) {
 			cb(res);
 		});
 	}

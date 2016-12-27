ea27e6a6e3c32246d3cad0783852da3890e46e31
Jason Sinn <jrs341@me.com>
2016-12-27 13:55:20 -0600

    updated with solution

--- db/schema.sql
+++ db/schema.sql
@@ -1,14 +1,13 @@
-### Schema
-
-CREATE DATABASE burgers_db;
-
+CREATE DATABASE IF NOT EXISTS burgers_db;
 USE burgers_db;
 
-CREATE TABLE burgers
-(
-	id int NOT NULL AUTO_INCREMENT,
-	burger_name varchar(255) NOT NULL,
-	devoured boolean NOT NULL DEFAULT false,
-	date_created DATETIME NOT NULL DEFAULT NOW(),
-	PRIMARY KEY (id)
+# If the table already exists, remove it before trying to create the table again
+DROP TABLE IF EXISTS burgers;
+
+# Create the burgers table
+CREATE TABLE burgers (
+id int NOT NULL AUTO_INCREMENT,
+burger_name varchar(255) NOT NULL,
+devoured BOOL DEFAULT false,
+PRIMARY KEY (id)
 );
